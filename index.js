0bdd5afa33f9fe8309e7616c230680d67dc14cfe
Isaac Reji <isaac.reji@kalvium.community>
2025-01-03 09:55:44 +0530

    assignment1 on connecting the mongodb database with the express server

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose=require('mongoose');
+require('dotenv').config()
 const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
-
+const mongoURL=process.env.MONGO_URL
 app.use(express.static('static'));
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to database');
+})
+.catch((error) => {
+  console.error('Error connecting to database:', error.message);
+});
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
